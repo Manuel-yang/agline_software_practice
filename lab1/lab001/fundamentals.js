63fd57a477f9abf3b0cce2f68d6441f4185977bc
YanAemons <731270812@qq.com>
2022-09-13 10:26:45 +0100

    Arrays

--- lab1/lab001/fundamentals.js
+++ lab1/lab001/fundamentals.js
@@ -1,9 +1,27 @@
-const aCar = { 
-  owner: "Joe Bloggs", 
+// const aCar = { 
+//   owner: "Joe Bloggs", 
+//   type: 'Toyota Corolla 1.8', 
+//   registration: '201WD1058',
+//   year: "201",
+//   countyCode: "WD",
+//   number: "1058"
+// };
+
+// console.log("Reg.=" + aCar.year + "-" + aCar.countyCode + "-" + aCar.number);
+
+const aCar = {
+  owner: "Joe Bloggs",
+  address: "3 Walkers Lane",
+  previous_owners: [
+    // "Pat Smith - 1 Main Street",
+    // "Sheila Dwyer - 2 High Street"
+    {name: "Pat Smith", address: "1 Main Street"},
+    {name: "Sheila Dwyer", address: "2 High Street"}
+  ],
   type: 'Toyota Corolla 1.8', 
   registration: '201WD1058',
   year: "201",
   countyCode: "WD",
   number: "1058"
 };
-console.log("Reg.=" + aCar.year + "-" + aCar.countyCode + "-" + aCar.number);
+console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address )
