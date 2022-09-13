c9d6d80bd93aa49d3ec20f6c47dd624dbeaf98a1
YanAemons <731270812@qq.com>
2022-09-13 10:20:36 +0100

    Nested objects.

--- lab1/lab001/fundamentals.js
+++ lab1/lab001/fundamentals.js
@@ -1,2 +1,9 @@
-const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
-console.log(aCar.owner);
+const aCar = { 
+  owner: "Joe Bloggs", 
+  type: 'Toyota Corolla 1.8', 
+  registration: '201WD1058',
+  year: "201",
+  countyCode: "WD",
+  number: "1058"
+};
+console.log("Reg.=" + aCar.year + "-" + aCar.countyCode + "-" + aCar.number);
