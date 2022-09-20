bf5c6d67185999255099373ec4c8c4aab1a90dac
YanAemons <731270812@qq.com>
2022-09-20 10:40:23 +0100

    Exercise2

--- functionslab/exercise2.js
+++ functionslab/exercise2.js
@@ -2,9 +2,11 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
      const completed = json // Complete this code
+     const todoTitles = []
      completed.forEach( (todo, index) => {
-      console.log(`${todo.title} - ${todo.completed}`)
+      todoTitles.push(todo.title)
     })
+    console.log(todoTitles)
   })
   .catch(function(err) { 
     console.log(err);
